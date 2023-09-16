f8e8b80536dcb901e52859efb5832c317d5f2176
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2023-09-16 15:36:36 +1000

    format code

--- src/App.js
+++ src/App.js
@@ -1,88 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
-
+/*================= mapping rendering data =======================*/
 
 function App() {
+  const item = [
+    {
+      name: "james",
+      age: "25",
+      born: "1998",
+    },
+    {
+      name: "vivi",
+      age: "26",
+      born: "1997",
+    },
+    {
+      name: "whissely",
+      age: "18",
+      born: "2003",
+    },
+  ];
 
-const item = [{
-  
+const myList = item.map((person) => (
+    <li>
+      {person.name} | {person.age} | {person.born}
+    </li>
+  ));
+  /*================= toglle show and hide button =======================*/
 
-  name:"james",
-  age:"25",
-  born:"1998",
-},
-{
-name:"vivi",
-  age:"26",
-  born:"1997",
+  const [show, setShow] = useState(true);
 
+  /*================= 2 binding data type =======================*/
 
-},
-{
-name:"whissely",
-  age:"18",
-  born:"2003",
+const [text, showText] = useState ('');
 
-},
-];
-
-const myList = item.map((person) => <li>{person.name} | {person.age} | {person.born}</li>
-
-);
-
-const [show, setShow] = useState (true);
-
-const [inputValue, setInputValue] = useState('');
-
-
-const handleInputChange = (event) => {
-  setInputValue(event.target.value);
+const handleChange = (event) => {
+  showText(event.target.value);
 };
- 
-
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-       Start Practice
-        </h1>
-<h2>Array map</h2>
-<div className='test'>{myList}</div>
+        <h1>Start Practice</h1>
+        <h2>Array map</h2>
+        <div className="test">{myList}</div>
 
+        <h2>show and hide</h2>
+        <div className="test2">
+          <button onClick={() => setShow(!show)}>
+            {" "}
+            {show ? "show toggle" : "hide toggle"}{" "}
+          </button>
 
-<h2>show and hide</h2>
-<div className='test2'>
+          {show && <span>test shown</span>}
+        </div>
 
-<button onClick={() => setShow(!show)}> { show ? "show toggle" : "hide toggle"} </button>
+        <h2> 2 wyas data Binding</h2>
 
-{show && <span>test shown</span>}
-
-</div>
-
-
-
-<h2> 2 wyas data Binding</h2>
-
-
-<div className='test3'>
-
-<div>
-      {/* Controlled input element */}
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-      />
-
-      {/* Display the current input value */}
-      <p>Input Value: {inputValue}</p>
-    </div>
-
-  
-</div>
+        <div className="test3">
+          <input type="text" onChange={handleChange} value={text}></input>
+    <p>type:{text}</p>
+        </div>
 
         <a
           className="App-link"
